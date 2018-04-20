30520966e204dc2571c313f0c3696915e362c40f
rohitdumbre86 <rohitdumbre86@gmail.com>
2018-04-20 07:52:00 -0500

    iterative fractals

--- Full Stack Web Development/Fundamentals/draw_fractals.py
+++ Full Stack Web Development/Fundamentals/draw_fractals.py
@@ -1,63 +1,66 @@
 import turtle
 
 
-def draw_triangle(x,y, length) :
-  tuts = turtle.Turtle()
-  tuts.hideturtle()
-  tuts.shape("turtle")
-  tuts.color("green")
+def draw_triangle(x,y, length, initangle) :
+  
+  tuts = initialize_turtle("turtle","green",initangle)
+  
   tuts.begin_fill()
   tuts.setpos(x,y)
-  tuts.fd(length)
-  tuts.left(120)
-  tuts.fd(length)
-  tuts.right(240)
-  tuts.fd(length)
-  tuts.left(120)
+
+  for i in range(1,4):
+    tuts.fd(length)
+    tuts.left(120)
+
   tuts.end_fill()
 
-def draw_inverse_triangle(x,y,length):  
+def draw_inverse_triangle(x,y,length,initangle):  
+  tuts = initialize_turtle("turtle","white",initangle)
+  
+  tuts.begin_fill()
+  tuts.setpos(x,y)
+  tuts.left(60)
+
+  for i in range(1,3):
+    tuts.fd(length)
+    tuts.left(120)
+
+  tuts.fd(length)
+  tuts.end_fill()
+
+
+def initialize_turtle(shape,color,initangle):
+  
   tuts = turtle.Turtle()
   tuts.hideturtle()
-  tuts.shape("turtle")  
-  tuts.color("white")
-  tuts.begin_fill()
-  tuts.setx(x)
-  tuts.left(60)
-  tuts.fd(length)
-  tuts.left(120)
-  tuts.fd(length)
-  tuts.left(120)
-  tuts.fd(length)
-  
-  tuts.end_fill()
-
+  tuts.shape(shape)
+  tuts.color(color)
+  tuts.hideturtle()
+  tuts.left(initangle)
     
-def draw_fractals(x,y,length,originalL):
+  return tuts
     
- if (length < originalL/4):
-   return
-
- draw_triangle(0,0,length)
- x = length/2
- length = length/2
- draw_inverse_triangle(x,y,length)
-
- draw_fractals(x,y,length,originalL)
- draw_fractals(x+x/2,y,length,originalL)
-  
-
-
-
-
+def draw_fractals(x,y,length,initangle):
     
+ draw_triangle(x,y,length, initangle)
+ draw_inverse_triangle(x+length/2,y,length/2, initangle)
+
 
 window = turtle.Screen()
 window.bgcolor("white")
 
 
+draw_fractals(0,0,200,0)
+draw_fractals(0,0,100,0)
+draw_fractals(0,0,50,0)
+draw_fractals(50,0,50,0)
+draw_fractals(100,0,100,0)
+draw_fractals(100,0,50,0)
+draw_fractals(150,0,50,0)
 
-draw_fractals(0,0,200,200)
+
+
+
 window.exitonclick()
 
 
